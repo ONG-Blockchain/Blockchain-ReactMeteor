ec05288a5c3812280fe02819eeeea13e8d07055a
jordimairena <jordim95@hotmail.com>
2018-11-29 15:49:37 -0600

    login frontend sin diseno

--- transparency-ong/imports/ui/Login/Login.js
+++ transparency-ong/imports/ui/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './Login.css';
 export default class Login extends React.Component {
-	render() {
-		return (
-			<h1> Hello World </h1>
-		)
-	}
+  render() {
+    
+    return (
+      <div className="body">
+        <h1 align="center"> Login </h1>
+        <div>
+          <label>User: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input type="password" />
+        </div>
+        <div className="btn">
+          <button>Register</button>
+          <button>Login</button>
+        </div>
+      </div>
+    )
+  }
 }
