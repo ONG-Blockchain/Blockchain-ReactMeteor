5dea279d3c52e4b585c7e5121c5312909042c524
miguepity <miguel.ardon@unitec.edu>
2018-12-03 15:41:48 -0600

    agregando register

--- transparency-ong/imports/ui/Login/Login.js
+++ transparency-ong/imports/ui/Login/Login.js
@@ -1,24 +1,102 @@
 import React from 'react';
 import './Login.css';
-export default class Login extends React.Component {
+
+class Login extends React.Component {
   render() {
-    
-    return (
-      <div className="body">
+
+    const login = (
+      <div className="content">
         <h1 align="center"> Login </h1>
         <div>
-          <label>User: </label>
+          <label>Usuario: </label>
           <input type="text" />
         </div>
         <div>
-          <label>Password: </label>
+          <label>Contraseña: </label>
           <input type="password" />
         </div>
         <div className="btn">
-          <button>Register</button>
           <button>Login</button>
         </div>
       </div>
+    );
+
+    const register = (
+      <div className="container">
+        <h1>Registrarse</h1>
+
+        <div className="container">
+        
+          <div className="row">
+        
+            <div className="col">
+              <label>Nombres: </label>
+              <input type="text"/>
+            </div>
+        
+            <div className="col">
+              <label>Apellidos: </label>
+              <input type="text"/>
+            </div>
+          </div>
+        
+          <div className="row">
+        
+            <div className="col">
+            <label>Usuario: </label>
+            <input type="text"/>
+            </div>
+        
+            <div className="col">
+            <label>Identidad: </label>
+            <input type="text"/>
+            </div>
+          </div>
+        
+          <div className="row">
+        
+            <div className="col">
+            <label>Contraseña: </label>
+            <input type="password"/>
+            </div>
+        
+            <div className="col">
+            <label>Confirmar Contraseña: </label>
+            <input type="password"/>
+            </div>
+          </div>
+        
+          <div className="row">
+        
+            <div className="col">
+            <label>Telefono/Celular: </label>
+            <input type="text"/>
+            </div>
+        
+            <div className="col">
+            <label>Correo electronico: </label>
+            <input type="e-mail"/>
+            </div>
+          </div>
+        
+        </div>
+        <button className="btn btn-dark">Registrar</button>
+      </div>
+    );
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            {login}
+          </div>
+          <div className="col">
+            {register}
+          </div>
+        </div>
+      </div>
+
     )
   }
 }
+export default Login;
