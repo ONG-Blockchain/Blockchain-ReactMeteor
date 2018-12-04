f4de2668da105dbe1a9adcb0765db1b06fcfeb68
miguepity <miguel.ardon@unitec.edu>
2018-12-04 11:07:21 -0600

    modificando  inputs

--- transparency-ong/imports/ui/Login/Login.js
+++ transparency-ong/imports/ui/Login/Login.js
@@ -5,7 +5,7 @@ class Login extends React.Component {
   render() {
 
     const login = (
-      <div className="content">
+      <div className="container">
         <h1 align="center"> Login </h1>
         <div>
           <label>Usuario: </label>
@@ -23,18 +23,18 @@ class Login extends React.Component {
 
     const register = (
       <div className="container">
-        <h1>Registrarse</h1>
+        <h1 className="registertitle">Registrarse</h1>
 
         <div className="container">
         
           <div className="row">
         
-            <div className="col">
+            <div className="col-sm">
               <label>Nombres: </label>
               <input type="text"/>
             </div>
         
-            <div className="col">
+            <div className="col-sm">
               <label>Apellidos: </label>
               <input type="text"/>
             </div>
@@ -42,12 +42,12 @@ class Login extends React.Component {
         
           <div className="row">
         
-            <div className="col">
+            <div className="col-sm">
             <label>Usuario: </label>
             <input type="text"/>
             </div>
         
-            <div className="col">
+            <div className="col-sm">
             <label>Identidad: </label>
             <input type="text"/>
             </div>
@@ -55,12 +55,12 @@ class Login extends React.Component {
         
           <div className="row">
         
-            <div className="col">
+            <div className="col-sm">
             <label>Contraseña: </label>
             <input type="password"/>
             </div>
         
-            <div className="col">
+            <div className="col-sm">
             <label>Confirmar Contraseña: </label>
             <input type="password"/>
             </div>
@@ -68,24 +68,24 @@ class Login extends React.Component {
         
           <div className="row">
         
-            <div className="col">
+            <div className="col-sm">
             <label>Telefono/Celular: </label>
             <input type="text"/>
             </div>
         
-            <div className="col">
+            <div className="col-sm">
             <label>Correo electronico: </label>
             <input type="e-mail"/>
             </div>
           </div>
         
         </div>
-        <button className="btn btn-dark">Registrar</button>
+        <button className="btn btn-dark registerbtn">Registrar</button>
       </div>
     );
 
     return (
-      <div className="container">
+      <div className="background container-fluid">
         <div className="row">
           <div className="col">
             {login}
